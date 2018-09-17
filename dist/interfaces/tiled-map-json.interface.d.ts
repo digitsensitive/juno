8c645ff6a2280138e963d554d84100a1267fc024
digitsensitive <digit.sensitivee@gmail.com>
2018-09-17 17:40:29 +0200

    update tiled-map-json interface

--- dist/interfaces/tiled-map-json.interface.d.ts
+++ dist/interfaces/tiled-map-json.interface.d.ts
@@ -4,11 +4,23 @@
  * @description  Juno: Tiled Map Json Interface
  * @license      {@link https://github.com/digitsensitive/juno-console/blob/master/license.txt|MIT License}
  */
+interface ILayer {
+    name: string;
+    type: string;
+    data: number[];
+    height: number;
+    width: number;
+    visible: boolean;
+    opacity: number;
+    x: number;
+    y: number;
+}
 export interface ITiledMapJson {
     mapHeight: number;
     mapWidth: number;
     tileWidth: number;
     tileHeight: number;
-    layers: any[];
+    layers: ILayer[];
     tilesets: any[];
 }
+export {};
